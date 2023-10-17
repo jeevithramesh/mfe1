b0f6883adbb257d1387d161bc20b2e5a86599485
Jeevith Ramesh <Jeevith>
2023-10-17 12:05:29 +0530

    added configs for MFE

---
+++ src/lib/modules/work-experience/work-experience.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { WorkExperienceComponent } from "./work-experience.component";
+import { WorkExperienceRoutingModule } from "./work-experience-routing.module";
+
+@NgModule({
+  declarations: [WorkExperienceComponent],
+  imports: [CommonModule, WorkExperienceRoutingModule],
+  exports: [],
+})
+export class WorkExperienceModule {}
